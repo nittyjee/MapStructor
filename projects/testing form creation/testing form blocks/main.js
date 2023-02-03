344f212051e4eb93dff3f5f891ebc1a815a9f376
ejiofor <ejioforched@gmail.com>
2023-02-03 18:29:58 +0300

    save As and editable.

--- projects/testing form creation/testing form blocks/main.js
+++ projects/testing form creation/testing form blocks/main.js
@@ -1,6 +1,25 @@
+let fileHandle;
+
 async function button(){
-  let [fileHandle] = await window.showOpenFilePicker();
+  [fileHandle] = await window.showOpenFilePicker();
 //   console.log(fileHandle.kind);
+
+///++++ IMP / Get the file with data
 let fileData = await fileHandle.getFile();
-   console.log(fileData);
+//console.log(fileData);
+
+// ++++ SEE TEXT
+let text = await fileData.text();
+//console.log(text);
+
+// ++++++ Edit TextArea
+textarea.innerText = text;
+}
+
+
+async function save(){
+    let stream = await fileHandle.createWritable();
+    await stream.write(textarea.innerText);
+    await stream.close();
+    
 }
