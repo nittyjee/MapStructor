cf5d994c5b13b8ef4cdc0ebc501bfb7ec38891b9
Vishapasar <77992836+Vishapasar@users.noreply.github.com>
2024-05-08 17:26:27 +0400

    Global Labels Layers

--- projects/globalhistorymap/js/timeLayers.js
+++ projects/globalhistorymap/js/timeLayers.js
@@ -5,13 +5,15 @@ function changeDate(unixDate) {
   var dateFilter = ["all", ["<=", "DayStart", date], [">=", "DayEnd", date]];
 
   //LAYERS FOR FILTERING
-  ["germany-highlighted", "germany-lines", "germany", "global-highlighted", "global-lines", "global"].forEach(id => {
+  ["germany-highlighted", "germany-lines", "germany", "global-highlighted", "global-lines", "global", "global-places", "global-labels"].forEach(id => {
     beforeMap.setFilter(id, dateFilter)
     afterMap.setFilter(id, dateFilter)
   })
 
+  /*
   beforeMap.setFilter("lot_events-bf43eb-left", dateFilter);
   afterMap.setFilter("lot_events-bf43eb-right", dateFilter);
-
+  */
+  
   demoFilterRangeCalc();
 } //end function changeDate
